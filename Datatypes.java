4bf9ac6b97b70b0caf01e8d6366848d7faf2dca6
MuthonduG <muthondugithinji@gmail.com>
2025-02-11 23:51:47 -0500

    initial commit

---
+++ Datatypes.java
@@ -0,0 +1,21 @@
+class Datatypes {
+    public static void main(String[] args) {
+
+        //  integer types
+        byte aSingleByt = 100; // Stores whole numbers from -128 to 127
+        short aSmallNumber = 20000; // Stores whole numbers from -32,768 to 32,767
+        int anInteger = 2147483647; // Stores whole numbers from -2,147,483,648 to 2,147,483,647
+        long aLargeNumber = 9223372036854775807l; // Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+
+        // decimal types
+        double aDouble = 1.78181818918; // Stores fractional numbers. Sufficient for storing 15 to 16 decimal digits
+        float aFloat = 3.4028f; // Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+
+        // Boolean - Stores true or false values
+        boolean isWeekend = false; 
+        boolean isWeekday = true;
+
+        // characters - Stores a single character/letter or ASCII values or unicode sequence
+        char copyrightSymbol = '\u00A9';
+    }
+}
